305687f80cf118986a75e9a6b8910acb48917a45
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2023-07-09 14:32:23 -0300

    desktop sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,89 @@
 import Editor from "@/components/Editor/index";
+import { motion } from "framer-motion";
 import type { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
 import Image from "next/image";
-import { FiChevronsLeft, FiFileText, FiPlus, FiTrash } from "react-icons/fi";
+import { useState } from "react";
+import {
+  FiChevronsLeft,
+  FiChevronsRight,
+  FiFileText,
+  FiPlus,
+  FiTrash,
+} from "react-icons/fi";
 
 export default function Home({ session }: any) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const links = [
+    {
+      name: "Tailwindcss",
+    },
+    {
+      name: "Untitled",
+    },
+    {
+      name: "Untitled",
+    },
+    {
+      name: "Untitled",
+    },
+    {
+      name: "Untitled",
+    },
+    {
+      name: "Untitled",
+    },
+  ];
+
+  const itemVariants = {
+    closed: {
+      opacity: 0,
+    },
+    open: { opacity: 1 },
+  };
+
+  const sidebarVariants = {
+    closed: {
+      opacity: 0,
+      x: -200,
+    },
+    open: { opacity: 1, x: 0 },
+  };
+
   return (
     <div className="min-h-screen text-zinc-50">
-      <div className="min-h-screen mx-auto overflow-hidden w-full shadow-md bg-zinc-800 border-black/20 break-words md:grid md:grid-cols-[16rem_1fr]">
-        <div className="hidden md:block overflow-hidden bg-zinc-900 border-r border-r-zinc-700">
-          <aside className="fixed w-[16rem] ">
-            <div className="flex gap-4 group justify-between items-center px-6 py-6">
+      <div
+        className={`relative min-h-screen mx-auto overflow-hidden w-full shadow-md bg-zinc-800 border-black/20 break-words ${
+          isSidebarOpen && "md:grid md:grid-cols-[16rem_1fr]"
+        }`}
+      >
+        {!isSidebarOpen && (
+          <div className="flex items-center gap-4 mt-6">
+            <FiChevronsRight
+              className="absolute w-6 h-6 z-40 left-4  text-zinc-400 hover:bg-zinc-700 rounded-sm p-0.5 cursor-pointer"
+              onClick={() => setIsSidebarOpen(true)}
+            />
+
+            <div className="ml-16 flex gap-2 items-center">
+              <FiFileText className="w-4 h-4 text-zinc-400" />
+              <span>{links[0].name}</span>
+            </div>
+          </div>
+        )}
+        <motion.div
+          initial={"closed"}
+          animate={isSidebarOpen ? "open" : "closed"}
+          variants={sidebarVariants}
+          className="hidden md:block overflow-hidden bg-zinc-900 border-r border-r-zinc-700"
+        >
+          <motion.aside
+            initial="closed"
+            animate="open"
+            variants={itemVariants}
+            className="fixed w-[16rem]"
+          >
+            <motion.div className="flex gap-4 group justify-between items-center px-6 py-6">
               <div className="flex gap-4 group items-center">
                 <Image
                   src="https://lh3.googleusercontent.com/a-/AOh14GhL-wIhtHTZsei8k_MhcdQCQAtpnoFNbMm4BRBp=s96-c"
@@ -23,24 +96,22 @@ export default function Home({ session }: any) {
                 <span className="">Vinícius&apos;s Notes</span>
               </div>
 
-              <FiChevronsLeft className="w-6 h-6 text-zinc-400 hover:bg-zinc-700 rounded-sm p-0.5 cursor-pointer" />
-            </div>
-            <div className="flex flex-col gap-0.5 px-2">
-              <div className="mt-6 gap-2 flex justify-between items-center text-sm cursor-pointer bg-zinc-700 hover:bg-zinc-600 px-6 py-1 font-medium text-white rounded-sm transition-colors">
-                <div className="flex gap-2 items-center">
-                  <FiFileText className="w-4 h-4 text-zinc-400" />
-                  <span>TailwindCSS</span>
-                </div>
-                <FiTrash className="w-4 h-4 text-zinc-400" />
-              </div>
-
-              <div className="gap-2 flex justify-between items-center text-sm cursor-pointer hover:bg-zinc-600 px-6 py-1 text-zinc-400 rounded-sm transition-colors">
-                <div className="flex gap-2 items-center">
-                  <FiFileText className="w-4 h-4" />
-                  <span>Untitled</span>
-                </div>
-                <FiTrash className="w-4 h-4" />
-              </div>
+              <FiChevronsLeft
+                className="w-6 h-6 text-zinc-400 hover:bg-zinc-700 rounded-sm p-0.5 cursor-pointer"
+                onClick={() => setIsSidebarOpen(false)}
+              />
+            </motion.div>
+            <motion.div className="flex flex-col gap-0.5 px-2">
+              {links &&
+                links.map((link) => (
+                  <motion.div className="first:mt-6 first:bg-zinc-700 gap-2 flex justify-between items-center text-sm cursor-pointer bg-zinc-900 hover:bg-zinc-600 px-6 py-1 font-medium text-white rounded-sm transition-colors">
+                    <div className="flex gap-2 items-center">
+                      <FiFileText className="w-4 h-4 text-zinc-400" />
+                      <span>{link.name}</span>
+                    </div>
+                    <FiTrash className="w-4 h-4 text-zinc-400" />
+                  </motion.div>
+                ))}
 
               <div className="mt-1 gap-2 flex justify-between items-center text-sm cursor-pointer hover:bg-zinc-600 px-6 py-1 text-zinc-400 rounded-sm transition-colors">
                 <div className="flex gap-2 items-center">
@@ -48,10 +119,19 @@ export default function Home({ session }: any) {
                   <span className="font-medium">Add a page</span>
                 </div>
               </div>
+            </motion.div>
+          </motion.aside>
+        </motion.div>
+        <main className="px-20 py-4">
+          {isSidebarOpen && (
+            <div className="flex items-center gap-4 mt-2">
+              <div className="flex gap-2 items-center">
+                <FiFileText className="w-4 h-4 text-zinc-400" />
+                <span>{links[0].name}</span>
+              </div>
             </div>
-          </aside>
-        </div>
-        <main className="px-8 py-4">
+          )}
+
           <Editor />
         </main>
       </div>
