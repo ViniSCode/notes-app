0d31d411c2d71a3913f8c958c86523cec3166c86
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2023-07-22 12:10:45 -0300

    remove next auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,28 +1,30 @@
-import NextAuth from "next-auth/next";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
+function test() {}
 
-export default NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID!,
-      clientSecret: process.env.GOOGLE_SECRET!,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-  ],
+// import NextAuth from "next-auth/next";
+// import GithubProvider from "next-auth/providers/github";
+// import GoogleProvider from "next-auth/providers/google";
 
-  callbacks: {
-    async signIn({ user, account, profile }) {
-      const email = user.email!;
-      const name = user.name!;
+// export default NextAuth({
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID!,
+//       clientSecret: process.env.GOOGLE_SECRET!,
+//     }),
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID!,
+//       clientSecret: process.env.GITHUB_SECRET!,
+//     }),
+//   ],
 
-      return true;
-    },
-  },
-  jwt: {
-    secret: process.env.JWT_SECRET!,
-  },
-});
+//   callbacks: {
+//     async signIn({ user, account, profile }) {
+//       const email = user.email!;
+//       const name = user.name!;
+
+//       return true;
+//     },
+//   },
+//   jwt: {
+//     secret: process.env.JWT_SECRET!,
+//   },
+// });
